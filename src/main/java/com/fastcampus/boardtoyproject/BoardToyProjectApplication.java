430bd873986d710f0287b4f1e22a7c13d399ed77
snacktime <tth8181@gmail.com>
2024-05-11 22:40:21 +0900

    Create spring project
    스프링 설정

---
+++ src/main/java/com/fastcampus/boardtoyproject/BoardToyProjectApplication.java
@@ -0,0 +1,13 @@
+package com.fastcampus.boardtoyproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BoardToyProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BoardToyProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/fastcampus/boardtoyproject/BoardToyProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fastcampus.boardtoyproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BoardToyProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
