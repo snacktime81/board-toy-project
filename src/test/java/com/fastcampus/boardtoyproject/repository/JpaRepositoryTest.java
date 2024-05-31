b9c41e73c90f50d93d3dc6a377066964f0765b20
snacktime <tth8181@gmail.com>
2024-05-31 14:33:26 +0900

    #2 - JPA test코드에 newline 추가
    EOF가 안생기게 마지막에 newline추가

--- src/test/java/com/fastcampus/boardtoyproject/repository/JpaRepositoryTest.java
+++ src/test/java/com/fastcampus/boardtoyproject/repository/JpaRepositoryTest.java
@@ -88,6 +88,4 @@ class JpaRepositoryTest {
         assertThat(articleRepository.count()).isEqualTo(previousArticleCount - 1);
         assertThat(articleCommentRepository.count()).isEqualTo(previousArticleCommentCount - deletedCommentsSize);
     }
-
-
 }
