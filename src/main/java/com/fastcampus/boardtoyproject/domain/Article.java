fa54d60f525d6bdfb76a18140e697157b0a80c0f
snacktime <tth8181@gmail.com>
2024-05-31 14:23:51 +0900

    #2 - 도메인 코드를 엔티티로 변
    1 도메인 클래스 == 1테이블 매칭 되도록 설계했다. 생성자와 'equals()', 'hashcode()'등이 도메인 맥락에 맞게 설정함

---
+++ src/main/java/com/fastcampus/boardtoyproject/domain/Article.java
@@ -0,0 +1,85 @@
+package com.fastcampus.boardtoyproject.domain;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+import org.springframework.data.annotation.CreatedBy;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedBy;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import java.time.LocalDateTime;
+import java.util.LinkedHashSet;
+import java.util.Objects;
+import java.util.Set;
+
+@Getter
+@ToString
+@Table(indexes = {
+        @Index(columnList = "title")
+        , @Index(columnList = "hashtag")
+        , @Index(columnList = "createdAt")
+        , @Index(columnList = "createdBy")
+})
+@EntityListeners(AuditingEntityListener.class)
+@Entity
+public class Article {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Setter
+    @Column(nullable = false, length = 10000)
+    private String content;
+    @Setter
+    @Column(nullable = false)
+    private String title;
+    @Setter
+    private String hashtag;
+
+    @OrderBy("id")
+    @OneToMany(cascade = CascadeType.ALL, mappedBy = "article")
+    private final Set<ArticleComment> articleComments = new LinkedHashSet<>();
+
+    @CreatedDate
+    @Column(nullable = false)
+    private LocalDateTime createdAt;
+    @CreatedBy
+    @Column(nullable = false, length = 100)
+    private String createdBy;
+    @LastModifiedDate
+    @Column(nullable = false)
+    private LocalDateTime modifiedAt;
+    @LastModifiedBy
+    @Column(nullable = false, length = 100)
+    private String modifiedBy;
+
+    protected Article() {
+    }
+
+    private Article(String content, String title, String hashtag) {
+        this.content = content;
+        this.title = title;
+        this.hashtag = hashtag;
+    }
+
+    public static Article of(String content, String title, String hashtag) {
+        return new Article(content, title, hashtag);
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Article article)) return false;
+        return id != null && Objects.equals(id, article.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
+    }
+}
