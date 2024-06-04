dbedb178792f52f7b6b09628c9e145a6a5a69ce1
snacktime <tth8181@gmail.com>
2024-06-04 15:51:16 +0900

    #1 - Data REST 설정

--- src/main/java/com/fastcampus/boardtoyproject/repository/ArticleRepository.java
+++ src/main/java/com/fastcampus/boardtoyproject/repository/ArticleRepository.java
@@ -3,9 +3,11 @@ package com.fastcampus.boardtoyproject.repository;
 import com.fastcampus.boardtoyproject.domain.Article;
 import org.apache.catalina.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
 import javax.swing.text.html.Option;
 import java.util.Optional;
 
+@RepositoryRestResource
 public interface ArticleRepository extends JpaRepository<Article, Long> {
 }
