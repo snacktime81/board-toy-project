bc42726c46f8e863cde26c0e340c09c1c82075e0
snacktime <tth8181@gmail.com>
2024-06-04 16:35:55 +0900

    #1 - Spring Data REST 테스트 제외 처리

--- src/test/java/com/fastcampus/boardtoyproject/controller/DataRestTest.java
+++ src/test/java/com/fastcampus/boardtoyproject/controller/DataRestTest.java
@@ -1,5 +1,6 @@
 package com.fastcampus.boardtoyproject.controller;
 
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -15,6 +16,7 @@ import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 
+@Disabled("Spring Data REST 통합테스트는 불필요하므로 제외시킴")
 @DisplayName("Data REST - API test")
 @Transactional
 @AutoConfigureMockMvc
